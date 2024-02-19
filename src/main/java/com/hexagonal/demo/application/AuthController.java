cb84a6b9d17d0f4ffd61bcdd28e04ea90e41d108
abderrahman <prof.abder@gmail.com>
2024-02-19 18:11:27 +0100

    cross origin

--- src/main/java/com/hexagonal/demo/application/AuthController.java
+++ src/main/java/com/hexagonal/demo/application/AuthController.java
@@ -6,16 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthenticationService authenticationService;
